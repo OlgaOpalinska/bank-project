8495bcffe4fbc3f9fdceb046629a01e6c60717ec
OlgaOpalinska <olgaopalinska@wp.pl>
2021-06-03 00:03:43 +0200

    Add post mapping

--- src/main/java/org/kaczucha/controller/BankController.java
+++ src/main/java/org/kaczucha/controller/BankController.java
@@ -3,9 +3,10 @@ package org.kaczucha.controller;
 import lombok.RequiredArgsConstructor;
 import org.kaczucha.repository.entity.Client;
 import org.kaczucha.service.BankService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,7 +14,16 @@ public class BankController {
     private final BankService service;
 
     @GetMapping("/api/user")
-    public Client findByEmail(@RequestParam String email) {
-        return service.findByEmail(email);
+    public ResponseEntity<Client> findByEmail(@RequestParam String email) {
+        final Client client = service.findByEmail(email);
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("EXAMPLE_HEADER", "DUMMY_VALUE");
+        return new ResponseEntity<>(client, httpHeaders, HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/api/user")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createClient(@RequestBody Client client) {
+        service.save(client);
     }
 }
